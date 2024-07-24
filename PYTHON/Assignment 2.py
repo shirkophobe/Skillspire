b320a707db3ad51e39236c2551db4ad8bb085b50
Shirkophobe <abunadhra@gmail.com>
2024-07-23 22:18:41 -0700

    Created zip files for Assignment 2 folder and files.

---
+++ PYTHON/Assignment 2.py
@@ -0,0 +1,9 @@
+#Create a list containing 10 elements. Print out the last 5 elements of that list.
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(a[5:])
+
+#Using the same list. Print out all the elements between index 3 and 10.
+
+print(a[2:10])
