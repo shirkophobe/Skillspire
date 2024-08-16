bcc2c3fafd45badb448f0f936b0e68f7c2ab50d5
Shirkophobe <abunadhra@gmail.com>
2024-08-15 17:20:23 -0700

    Modified code.

--- ALGORITHMS/algo4.py
+++ ALGORITHMS/algo4.py
@@ -1,16 +1,16 @@
 '''Count Non-Spaces. Create a function that  accepts a string and returns the number 
-of non-space characters found in the string. For example, given ”lol cool dude", 
+of non-space characters found in the string. For example, given "lol cool dude", 
 return 11 (not 13). Remember NO BUILT IN FUNCTIONS.'''
 
-def count_non_space_chars(input_string):
+def count_non_space_characters(input_string):
     count = 0
     
-    for char in input_string:
-        if char != ' ':
+    for character in input_string:
+        if character != " ":
             count += 1
     
     return count
 
 
 test_string = "lol cool dude"
-print("The number of non-space characters is:", count_non_space_chars(test_string))
+print("The number of non-space characters is:", count_non_space_characters(test_string))
