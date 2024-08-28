c0247ce9c3b5ce10d645d1df9ed8175813662d5a
Shirkophobe <abunadhra@gmail.com>
2024-08-27 18:40:56 -0700

    Modified code.

--- FLASK/demo/app.py
+++ FLASK/demo/app.py
@@ -20,6 +20,16 @@ class Customer(db.Model):
 def default():
     return render_template("form.html")
 
+
+@app.route('/addcustomer', methods = ['GET', 'POST'])
+    
+def addcustomer():
+    customer = Customer( name = "Customer Name", phone_number = 5555555555, country = "USA")
+
+    db.session.add(customer)
+    
+    db.session.commit()
+    
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
