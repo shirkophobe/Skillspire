b3743982eaac24feca5ca4c3a7b49064ab9ed409
Shirkophobe <abunadhra@gmail.com>
2024-08-27 17:44:09 -0700

    Imported flask_sqlalchemy

--- FLASK/demo/app.py
+++ FLASK/demo/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session  
+from flask_sqlalchemy import SQLAlchemy
  
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+app.secret_key = 'secret'
  
-@app.route('/test')
+@app.route('/')
  
 def root():
-    return render_template("index.html")
+    return render_template("form.html")
 
 if __name__ == "__main__":
     app.run(debug = True, port = 5500)
