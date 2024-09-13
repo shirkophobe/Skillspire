68980334d7c264d261b4f6617873dea0011e8e38
Shirkophobe <abunadhra@gmail.com>
2024-09-12 18:11:49 -0700

    Modifed code in first_project. Created templates folder with index.html file in first_project folder.

--- PYTHON/Django/first_project/first_app/urls.py
+++ PYTHON/Django/first_project/first_app/urls.py
@@ -4,9 +4,7 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
-
-path(r'', views.index)
-
+    path(r'', views.index),
+    path('displayinfo', views.displayinfo)
 ]
--- PYTHON/Django/first_project/first_app/views.py
+++ PYTHON/Django/first_project/first_app/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("HELLO WORLD")
+    return render(request, "index.html", context = {})
+
+
+def displayinfo(request):
+    name = "James"
+    food = "Thai Cusine"
+    movie = "Lawrence of Arabia"
+    music = "Jazz"
+    
+    return HttpResponse(f'Name: {name} Favorite Food: {food} Movie: {movie} Music Genre: {music}')
