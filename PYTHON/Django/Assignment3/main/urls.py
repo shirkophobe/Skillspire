bac348f6f35b12e9fc58e3f7e8d9f22fca719a49
Shirkophobe <abunadhra@gmail.com>
2024-09-16 17:27:16 -0700

    Modified code in Assignment 3. Created .zip file of Assignment 3 called Django Assignment 3.

--- PYTHON/Django/Assignment3/main/urls.py
+++ PYTHON/Django/Assignment3/main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('display-name/<str:name>', views.displayname, name='display-name'),
+    path('display-name/<str:person_name>/', views.displayname, name='display-name'),
     path('display-food/<str:food>', views.displayfood, name='display-food'),
     path('display-vacation/<str:vacation>', views.displayvacation, name='display-vacation')
 ]
--- PYTHON/Django/Assignment3/main/views.py
+++ PYTHON/Django/Assignment3/main/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
     return render(request, "index.html")
  
-def displayname(request, name):
-   return render(request, "name.html" , context = { "name": name } ) 
+def displayname(request, person_name):
+   return render(request, "name.html" , context = { "name": person_name } ) 
 
 def displayfood(request, food):
    return render(request, "food.html" , context = { "food": food } ) 
 
 def displayvacation(request, vacation):
    return render(request, "vacation.html" , context = { "vacation": vacation } )  
+
+
