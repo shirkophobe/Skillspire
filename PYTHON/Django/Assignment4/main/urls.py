f92e4c9e8af6b8fd9e3ac0f822c629a59f810aa8
Shirkophobe <abunadhra@gmail.com>
2024-09-16 19:08:29 -0700

    Modified code

--- PYTHON/Django/Assignment4/main/urls.py
+++ PYTHON/Django/Assignment4/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('display-info/', views.display_info, name='display_info'),
+    path('submit-form/', views.submit_form, name='submit_form'),
 ]
--- PYTHON/Django/Assignment4/main/views.py
+++ PYTHON/Django/Assignment4/main/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 
+# Parts 1 & 2
 def display_info(request):
     context = {
         'first_name': 'James',
         'last_name': 'Browne',
         'favorite_food': 'Thai Cuisine',
-        'favorite_vacation': 'Bora Bora'
+        'favorite_vacation': 'Bora Bora',
+        'numbers': range(1, 11)  
     }
     return render(request, 'display_info.html', context)
+
+# Part 3 
+def submit_form(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        favorite_food = request.POST.get('favorite_food')
+
+        context = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'favorite_food': favorite_food
+        }
+        return render(request, 'submitted_data.html', context)
+    return render(request, 'form.html')
+
