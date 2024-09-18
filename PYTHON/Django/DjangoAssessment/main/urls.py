b2c5f90b43fa04923214a191f8c3a4b99b26d312
Shirkophobe <abunadhra@gmail.com>
2024-09-17 18:28:33 -0700

    Modified code

--- PYTHON/Django/DjangoAssessment/main/urls.py
+++ PYTHON/Django/DjangoAssessment/main/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-
+    path('', views.home, name='home'), #Part 1
+    path('about/', views.about, name='about'), #Part 1
+    path('greet/<str:name>/', views.greet, name='greet'),  # Part 2
+    path('contact/', views.contact, name='contact'), #Part 3
+    path('submit_contact/', views.submit_contact, name='submit_contact'), #Part 3
 ]
--- PYTHON/Django/DjangoAssessment/main/views.py
+++ PYTHON/Django/DjangoAssessment/main/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+# Part 1
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
-    
+#Part 2
+def greet(request, name):
+    return render(request, 'index.html', {'name': name}) 
+
+# Part 3
+def contact(request):
+    return render(request, 'contact.html')
+
+def submit_contact(request):
+    if request.method == 'POST':
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        if not firstname or not lastname or not email or not phone or not message:
+            error_message = "All fields are required."
+            return render(request, 'contact.html', {'error': error_message})
+
+        return render(request, 'confirmation.html', {
+            'firstname': firstname,
+            'lastname': lastname,
+            'email': email,
+            'phone': phone,
+            'message': message
+        })
+
+    return HttpResponse("Invalid request method.")
