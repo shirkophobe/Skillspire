f9e88c69e96bb76933a33a27757a95a7ca847ec9
Shirkophobe <abunadhra@gmail.com>
2024-09-17 17:27:14 -0700

    modified code

--- PYTHON/Django/DjangoAssessment/main/urls.py
+++ PYTHON/Django/DjangoAssessment/main/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 
 urlpatterns = [
-
-path(r'', views.index)
+    path(r'', views.index)
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 
 ]
