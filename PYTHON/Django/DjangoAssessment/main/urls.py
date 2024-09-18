cfde21277ad58aab6f23053043dd6cae0197260e
Shirkophobe <abunadhra@gmail.com>
2024-09-17 18:45:12 -0700

    Modified code

--- PYTHON/Django/DjangoAssessment/main/urls.py
+++ PYTHON/Django/DjangoAssessment/main/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'), #Part 1
-    path('about/', views.about, name='about'), #Part 1
+    path('', views.home, name='home'), # Part 1
+    path('about/', views.about, name='about'), # Part 1
     path('greet/<str:name>/', views.greet, name='greet'),  # Part 2
     path('contact/', views.contact, name='contact'), #Part 3
-    path('submit_contact/', views.submit_contact, name='submit_contact'), #Part 3
+    path('submit_contact/', views.submit_contact, name='submit_contact'), # Part 3
+    path('login/', views.login, name='login'), # Part 4
+    path('admin/', views.admin_panel, name='admin_panel'), # Part 4
 ]
