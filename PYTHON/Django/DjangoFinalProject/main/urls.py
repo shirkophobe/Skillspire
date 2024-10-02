a9cd2c34f5de119511b89c5f693ee7321f3b1a6e
Shirkophobe <abunadhra@gmail.com>
2024-10-02 02:04:09 -0700

    Modified code.

--- PYTHON/Django/DjangoFinalProject/main/urls.py
+++ PYTHON/Django/DjangoFinalProject/main/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.news_feed, name='home'),  
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
--- PYTHON/Django/DjangoFinalProject/main/views.py
+++ PYTHON/Django/DjangoFinalProject/main/views.py
@@ -10,30 +10,23 @@ from django.contrib import messages
 def home(request):
     return render(request, 'base.html')
 
-# Register view
 def register(request):
     form = UserCreationForm(request.POST or None)
     if request.method == 'POST' and form.is_valid():
         form.save()
-        # Add a success message
         messages.success(request, "Registration successful! Please log in.")
-        return redirect('home')  # Redirect to home after registration
+        return redirect('home')  
     return render(request, 'register.html', {'form': form, 'title': 'Register', 'button_text': 'Register'})
 
-
-
-# Login view
 def user_login(request):
     form = AuthenticationForm(request, data=request.POST)
     if request.method == 'POST' and form.is_valid():
         user = form.get_user()
         login(request, user)
-        return redirect('news_feed')  # Redirect to the news feed page after a successful login
+        return redirect('news_feed')  
 
     return render(request, 'login.html', {'form': form, 'title': 'Login', 'button_text': 'Login'})
 
-
-# News Feed (main page after login)
 @login_required
 def news_feed(request):
     if request.method == 'POST':
@@ -45,7 +38,6 @@ def news_feed(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'news_feed.html', {'posts': posts})
 
-# Edit Post
 @login_required
 def edit_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
@@ -61,7 +53,6 @@ def edit_post(request, post_id):
 
     return render(request, 'edit_post.html', {'post': post})
 
-# Delete Post
 @login_required
 def delete_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
